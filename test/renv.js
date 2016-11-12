4181bd7e4b4d7c34511e23efd35378ec9e0a4043
Maciej Adwent <maciekadwent@gmail.com>
2016-11-11 16:24:34 -0800

    Preserve old renv 2.x.x API, fix an object merge bug. Add more tests

--- test/renv.js
+++ test/renv.js
@@ -7,9 +7,36 @@ describe("renv", () => {
 
   describe("From a local JSON file", () => {
 
-    it("should load the common _ set", () => {
+    it("should get the common _ environment", () => {
       renv.getEnv(TESTFILE, [], (err, env) => {
         expect(env.COMMONVAR1).to.equal("abc123");
+      });
+    });
+
+    it("should get a environment with no stage", () => {
+      renv.getEnv(TESTFILE, ["project1"], (err, env) => {
+        expect(env.COMMONVAR1).to.equal("abc123");
+        expect(env.PROJECTVAR1).to.equal("abc123");
+        expect(Object.keys(env).length).to.equal(2);
+      });
+    });
+
+    it("should get a sub-environment", () => {
+      renv.getEnv(TESTFILE, ["project1.pr"], (err, env) => {
+        expect(env.COMMONVAR1).to.equal("abc123");
+        expect(env.PROJECTVAR1).to.equal("abc123");
+        expect(env.PROJECTVAR2).to.equal("def456");
+        expect(Object.keys(env).length).to.equal(3);
+      });
+    });
+
+    it("should merge two sub-environments in order", () => {
+      renv.getEnv(TESTFILE, ["project1.pr","project1.master"], (err, env) => {
+        expect(env.COMMONVAR1).to.equal("abc123");
+        expect(env.PROJECTVAR1).to.equal("abc123");
+        expect(env.PROJECTVAR2).to.equal("ghi789");
+        expect(env.PROJECTVAR3).to.equal("jkl012");
+        expect(Object.keys(env).length).to.equal(4);
       });
     });
 
